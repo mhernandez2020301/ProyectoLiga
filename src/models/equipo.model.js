a063c692965dfae346ab06c4025306c9c3e7884d
Marlón Gabriel Hernández Coromac <88806006+mhernandez2020301@users.noreply.github.com>
2022-05-08 16:57:58 -0600

    Update

--- src/models/equipo.model.js
+++ src/models/equipo.model.js
@@ -3,6 +3,13 @@ var Schema = mongoose.Schema;
 
 var EquipoSchema = Schema ({
     nombre: String,
+    golesF: Number,
+    golesC: Number,
+    Diferencia: Number,
+    partidosJugados: Number,
+    puntos: Number,
+    idUsuario: {type: Schema.Types.ObjectId, ref: 'Usuarios'},
+    idLiga: {type: Schema.Types.ObjectId, ref: 'Liga'}
 })
 
 module.exports = mongoose.model("Equipo", EquipoSchema);
